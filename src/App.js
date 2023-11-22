8f27dbc25f80379e287e681478d4c2586f8f64fd
Ujjwal <ujjwaldhariwal0@gmail.com>
2023-11-22 15:02:36 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -192,7 +192,7 @@ function App() {
 
             <UTCDatetime />
             <Link
-              href="https://github.com/Amin-Awinti"
+              href="https://github.com/https://github.com/Ujjwaldhariwal"
               target="_blank"
               underline="none"
               sx={{ display: 'flex' }}
--- src/api/OpenWeatherService.js
+++ src/api/OpenWeatherService.js
@@ -1,7 +1,9 @@
 const GEO_API_URL = 'https://wft-geo-db.p.rapidapi.com/v1/geo';
 
 const WEATHER_API_URL = 'https://api.openweathermap.org/data/2.5';
-const WEATHER_API_KEY = 'Your API KEY';
+const WEATHER_API_KEY = '230b48d0e6e0cbf202d35ba358137ec7';
+
+
 
 const GEO_API_OPTIONS = {
   method: 'GET',
